ed62e3ee542b2e2ff63c0e41954de7ce7dc19862
Iuliia <jultim141103@gmail.com>
2022-04-19 23:42:20 +0300

    first attempt

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,2 +1,21 @@
-import streamlit
-help(streamlit)
+import streamlit as st
+import numpy as np
+
+with st.echo(code_location='below'):
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
+    theta = np.arange(0.01, 2 * np.pi, 0.01)
+
+
+    def dipole_diagram(p):
+        f = 20 * np.log(((np.cos(np.pi * p * np.cos(theta)) - np.cos(p * np.pi)) / (np.sin(theta))) ** 2)
+        return f
+
+
+    length = 1
+    while length:
+        length = 0.5 #float(input("Введите безразмерный параметр l/lambda: "))
+        ax.plot(theta, dipole_diagram(length))
+    st.write(fig)
+
+
