d19c17c3dc61273f0e5d61cc7f8b30d9a418bc24
EgorNekludov <egor_nekludov@list.ru>
2022-04-24 16:38:08 +0300

    stramlit by Egor

--- stremlit_app.py
+++ stremlit_app.py
@@ -7,7 +7,15 @@ import json
 st.title('Калькулятор диаграмм направленности антенн')
 an_type = st.radio("Choose your antenna type (выберите тип антенны):", ('Horn', 'Dipole', 'Patch'))
 if an_type == 'Horn':
-    st.write('Введите ширину антенны "a", высоту антенны "b" и длину волны:')
+    st.write('Введите ширину антенны "a", высоту антенны "b" и длину волны "wl":')
     width = st.number_input('a', 0, None)
     height = st.number_input('b', 0, None)
     wave_length = st.number_input('l', 0, None)
+if an_type == 'Dipole':
+    st.write('Введите длину диполя "l" и длину волны "wl":')
+    dipole_length = st.number_input('l', 0, None)
+    wave_length = st.number_input('wl', 0, None)
+if an_type == 'Patch':
+    st.write('Введите параметр "w" и длину волны "wl":')
+    parameter = st.number_input('w', 0, None)
+    wave_length = st.number_input('wl', 0, None)
