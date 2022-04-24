b8b4f5995f70daecb386e52f062f1db8412c184e
iulia141 <90765293+iulia141@users.noreply.github.com>
2022-04-24 16:28:32 +0300

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+#import plotly.express as px
 from urllib.request import urlopen
 import json
 st.title('Калькулятор диаграмм направленности антенн')
