6f31bb8b1c9510ad07a61ff2cc4a37301d98bb1f
EgorNekludov <egor_nekludov@list.ru>
2022-04-25 23:10:41 +0300

    stramlit by Egor

--- stremlit_app.py
+++ stremlit_app.py
@@ -5,34 +5,69 @@ import numpy as np
 from urllib.request import urlopen
 import json
 import matplotlib.pyplot as plt
-st.title('Калькулятор диаграмм направленности антенн')
-an_type = st.radio("Choose your antenna type (выберите тип антенны):", ('Horn', 'Dipole', 'Patch'))
-if an_type == 'Horn':
-    st.write('Введите ширину антенны "a" в метрах, высоту антенны "b" в метрах и длину волны "wl" в метрах:')
-    width = st.number_input('a', 0.0, None)
-    height = st.number_input('b', 0.0, None)
-    wave_length = st.number_input('l', 0.0, None)
+st.title('Antenna radiation pattern calculator (Калькулятор диаграмм направленности антенн)')
+ln_type = st.radio("Choose your language (Выберите свой язык):", ('English', 'Русский'))
+if ln_type == 'Русский':
+    an_type = st.radio("Выберите тип антенны):", ('Horn', 'Dipole', 'Patch'))
+    if an_type == 'Horn':
+        st.write('Введите ширину антенны "a" в метрах, высоту антенны "b" в метрах и длину волны "wl" в метрах:')
+        width = st.number_input('a', 0.0, None)
+        height = st.number_input('b', 0.0, None)
+        wave_length = st.number_input('l', 0.0, None)
 
-if an_type == 'Dipole':
-    st.write('Введите длину диполя "l" в метрах и длину волны "wl" в метрах:')
-    dipole_length = st.number_input('l', 0.0, None)
-    wave_length = st.number_input('wl', 0.0, None)
+    if an_type == 'Dipole':
+        st.write('Введите длину диполя "l" в метрах и длину волны "wl" в метрах:')
+        dipole_length = st.number_input('l', 0.0, None)
+        wave_length = st.number_input('wl', 0.0, None)
 
-    theta = np.arange(0.001, 2 * np.pi, 0.001)
-    fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
+        theta = np.arange(0.001, 2 * np.pi, 0.001)
+        fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
 
-    def dipole_diagram(wl, l):
-        f = 20 * np.log(((np.cos(np.pi * (l / wl) * np.cos(theta)) - np.cos((l / wl) * np.pi)) / (np.sin(theta))) ** 2)
-        return f
+        def dipole_diagram(wl, l):
+            f = 20 * np.log(((np.cos(np.pi * (l / wl) * np.cos(theta)) - np.cos((l / wl) * np.pi)) / (np.sin(theta))) ** 2)
+            return f
 
-    ax.plot(theta, dipole_diagram(wave_length, dipole_length))
-    ax.set_theta_zero_location('N')
-    ax.set_rlabel_position(-90)
-    plt.thetagrids(range(0, 360, 30))
+        ax.plot(theta, dipole_diagram(wave_length, dipole_length))
+        ax.set_theta_zero_location('N')
+        ax.set_rlabel_position(-90)
+        plt.thetagrids(range(0, 360, 30))
 
-    st.pyplot(fig)
+        st.pyplot(fig)
 
-if an_type == 'Patch':
-    st.write('Введите параметр "w" и длину волны "wl" в метрах:')
-    parameter = st.number_input('w', 0.0, None)
-    wave_length = st.number_input('wl', 0.0, None)
+    if an_type == 'Patch':
+        st.write('Введите параметр "w" и длину волны "wl" в метрах:')
+        parameter = st.number_input('w', 0.0, None)
+        wave_length = st.number_input('wl', 0.0, None)
+if ln_type == 'English':
+    an_type = st.radio("Choose your antenna type):", ('Horn', 'Dipole', 'Patch'))
+    if an_type == 'Horn':
+        st.write('Enter antenna width "a" in meters, antenna height "b" in meters and wave length "wl" in meters:')
+        width = st.number_input('a', 0.0, None)
+        height = st.number_input('b', 0.0, None)
+        wave_length = st.number_input('l', 0.0, None)
+
+    if an_type == 'Dipole':
+        st.write('Enter dipole length "l" in meters and wave length "wl" in meters:')
+        dipole_length = st.number_input('l', 0.0, None)
+        wave_length = st.number_input('wl', 0.0, None)
+
+        theta = np.arange(0.001, 2 * np.pi, 0.001)
+        fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
+
+
+        def dipole_diagram(wl, l):
+            f = 20 * np.log(((np.cos(np.pi * (l / wl) * np.cos(theta)) - np.cos((l / wl) * np.pi)) / (np.sin(theta))) ** 2)
+            return f
+
+
+        ax.plot(theta, dipole_diagram(wave_length, dipole_length))
+        ax.set_theta_zero_location('N')
+        ax.set_rlabel_position(-90)
+        plt.thetagrids(range(0, 360, 30))
+
+        st.pyplot(fig)
+
+    if an_type == 'Patch':
+        st.write('Enter parameter "w" and wave length "wl" in meters:')
+        parameter = st.number_input('w', 0.0, None)
+        wave_length = st.number_input('wl', 0.0, None)
