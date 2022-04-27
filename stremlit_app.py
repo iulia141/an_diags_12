5c4ff0fc389b39e0f848a77f44ff6a5af0cc00e7
EgorNekludov <egor_nekludov@list.ru>
2022-04-27 20:44:35 +0300

    stramlit by Egor

--- stremlit_app.py
+++ stremlit_app.py
@@ -16,24 +16,27 @@ if ln_type == 'Русский':
         wave_length = st.number_input('l', 0.0, None)
 
     if an_type == 'Dipole':
+
         st.write('Введите длину диполя "l" в метрах и длину волны "wl" в метрах:')
         dipole_length = st.number_input('l', 0.0, None)
         wave_length = st.number_input('wl', 0.0, None)
+        try:
+            theta = np.arange(0.001, 2 * np.pi, 0.001)
+            fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
 
-        theta = np.arange(0.001, 2 * np.pi, 0.001)
-        fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
+            f = (np.cos(np.pi * (dipole_length / wave_length) * np.cos(theta)) - np.cos(
+                (dipole_length / wave_length) * np.pi)) / (np.sin(theta))
+            norm = f.max()
+            dipole_diagram = 20 * np.log((f / norm) ** 2)
 
-        f = (np.cos(np.pi * (dipole_length / wave_length) * np.cos(theta)) - np.cos(
-            (dipole_length / wave_length) * np.pi)) / (np.sin(theta))
-        norm = f.max()
-        dipole_diagram = 20 * np.log((f / norm) ** 2)
+            ax.plot(theta, dipole_diagram)
+            ax.set_theta_zero_location('N')
+            ax.set_rlabel_position(-90)
+            plt.thetagrids(range(0, 360, 30))
 
-        ax.plot(theta, dipole_diagram)
-        ax.set_theta_zero_location('N')
-        ax.set_rlabel_position(-90)
-        plt.thetagrids(range(0, 360, 30))
-
-        st.pyplot(fig)
+            st.pyplot(fig)
+        except:
+            st.write('Неверно введены данные')
 
     if an_type == 'Patch':
         st.write('Введите параметр "w" и длину волны "wl" в метрах:')
@@ -51,22 +54,23 @@ if ln_type == 'English':
         st.write('Enter dipole length "l" in meters and wave length "wl" in meters:')
         dipole_length = st.number_input('l', 0.0, None)
         wave_length = st.number_input('wl', 0.0, None)
+        try:
+            theta = np.arange(0.001, 2 * np.pi, 0.001)
+            fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
 
-        theta = np.arange(0.001, 2 * np.pi, 0.001)
-        fig, ax = plt.subplots(subplot_kw=dict(projection="polar"))
+            f = (np.cos(np.pi * (dipole_length / wave_length) * np.cos(theta)) - np.cos(
+                (dipole_length / wave_length) * np.pi)) / (np.sin(theta))
+            norm = f.max()
+            dipole_diagram = 20 * np.log((f / norm) ** 2)
 
-        f = (np.cos(np.pi * (dipole_length / wave_length) * np.cos(theta)) - np.cos(
-            (dipole_length / wave_length) * np.pi)) / (np.sin(theta))
-        norm = f.max()
-        dipole_diagram = 20 * np.log((f / norm) ** 2)
+            ax.plot(theta, dipole_diagram)
+            ax.set_theta_zero_location('N')
+            ax.set_rlabel_position(-90)
+            plt.thetagrids(range(0, 360, 30))
 
-        ax.plot(theta, dipole_diagram)
-        ax.set_theta_zero_location('N')
-        ax.set_rlabel_position(-90)
-        plt.thetagrids(range(0, 360, 30))
-
-        st.pyplot(fig)
-
+            st.pyplot(fig)
+        except:
+            st.write('Incorrect data entered')
     if an_type == 'Patch':
         st.write('Enter parameter "w" and wave length "wl" in meters:')
         parameter = st.number_input('w', 0.0, None)
